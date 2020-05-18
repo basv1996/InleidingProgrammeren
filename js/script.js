6034e20a7c34e3b66fb279e430795a00d1b3ae66
basv1996 <bas.vugts@gmail.com>
2020-05-18 21:07:37 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,47 +1,50 @@
+//bij versie 2 moet er data worden verwerkt
+
 "use strict";
 
 var scoreText = document.querySelector("#scoreBord");
-var levelText = document.querySelector("#level");
 var enemy = document.querySelector("#enemY");
+var levelText = document.querySelector("#level");
 var containerEl = document.querySelector("#container");
-var bodyEl = document.querySelector("body");
+var selectEl = document.querySelector("#colorSelect");
+
+var enemies = ['Black_enemy.svg', 'Yellow_enemy.svg', 'Red_enemy.svg', 'Blue_enemy.svg'];
 
 var punten = 0;
-var level = 1;
+var level = 1
+var newEnemy = 0;
+
+function playersChoice() {
+    var keuze = selectEl.selectedIndex;
+    enemy.src = "img/" + enemies[keuze];
+    console.log(enemy);
+console.log(enemies);
+console.log(enemy.src);
+}
 
 
 function updateScore() {
-    punten = punten + 1; // tel 1 punt bij de variablele punten op
-    scoreText.textContent = punten; // verander de tekst in de waarde "punten"
+    punten = punten + 1;
+    scoreText.textContent = punten;
 }
 
 function levelUp() {
-    if (punten >= 10) //als variabele punten groter of gelijk is aan 10 voer dan onderstaande code uit
-    {
-        punten = 0; //reset het punten aantal
-        scoreText.textContent = punten; // verander de tekst in de waarde "punten"
-        level = level + 1; // tel 1 bij de variabele level op
-        levelText.textContent = level;   // verander de tekst in de waarde "level" 
-    }   
-}
-
-function changeColour() {
-    if (level % 2) //als level een even getal is (deelbaar door 2 zonder rest getal)
-     {
-containerEl.className = ''; //dit zorgt ervoor de classname van het element wordt gereset naar niks
-        bodyEl.className = '';
-        containerEl.classList.add('rood'); // voeg de class rood aan het container element in de html toe
-        bodyEl.classList.add('groen'); // voeg de class groen aan het container element in de html toe
-     }
-    else 
-    {
-        containerEl.className = '';
-        bodyEl.className = '';
-        containerEl.classList.add('groen');
-        bodyEl.classList.add('rood');
+    if (punten >= 10) {
+        punten = 0;
+        scoreText.textContent = punten;
+        newEnemy = newEnemy + 1;
+        enemy.src = "img/" + enemies[newEnemy];
+        level = level + 1;
+        levelText.textContent = level;  
+    }
+    if (newEnemy >= enemies.length - 1) {
+        newEnemy = 0;
     }
 }
 
-enemy.addEventListener("click", updateScore); //als er op de enemy een click wordt gedetacteerd voer dan de functie updateScore uit
-enemy.addEventListener("click",levelUp); //als er op de enemy een click wordt gedetacteerd voer dan de functie levelUp uit
-enemy.addEventListener("click",changeColour); //als er op de enemy een click wordt gedetacteerd voer dan de functie changeColour uit
+enemy.addEventListener("click", () => {
+    updateScore();
+    levelUp();
+});
+
+selectEl.addEventListener("change", playersChoice);
